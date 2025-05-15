23b1b632c35d3320fd61c9f504e74185dcc8c4c3
myxi <myxi@envs.net>
2025-05-16 00:00:30 +0530

    feat: add typing (not static)

--- src/kisesi/__init__.py
+++ src/kisesi/__init__.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
 from logging import *
+import typing as t
 
 KISESI_DEFAULT_FORMAT_STRING = (
     "[%(asctime)s] %(levelname)s @ %(filename)s:%(lineno)d %(funcName)s :: %(message)s"
 )
 NOT_SET = NOTSET
 
+class LoggerLike(Logger):
+    def __getattr__(self, name: str): 
+        return getattr(self, name)
 
 class Color:
     RESET = "\033[0m"
@@ -52,37 +57,38 @@ class ColoredFormatter(Formatter):
         WARNING: "WARN",
     }
 
-    def format(self, record):
+    def format(self, record: LogRecord):
         level_color = self.LEVEL_COLORS.get(record.levelno, "")
         level_name = self.LEVEL_NAMES.get(record.levelno, record.levelname)
         record.levelname = f"{level_color}[{level_name}]{Color.RESET}"
         return super().format(record)
 
 
-def _get_default_kisesi_handler(fmt, datefmt):
+def _get_default_kisesi_handler(fmt: t.Any, datefmt: t.Any):
     handler = StreamHandler()
     handler.setFormatter(ColoredFormatter(fmt, datefmt=datefmt))
     return handler
 
 
-def _monkeypatch_logger(logger):
+def _monkeypatch_logger(logger: t.Any) -> LoggerLike | None:
     if not logger:
         return None
 
     # somewhere down the line in recursion, it returns a `logging.RootLogger`,
     # we handle that case here by not patching a `logging.RootLogger`
-    if not isinstance(logger, Logger):
+    if not isinstance(logger, Logger): # type: ignore
         return logger
 
     if getattr(logger, "__KISESI__", False):
-        return logger
+        return logger # type: ignore
 
+    logger = t.cast(Logger, t.Any)
     logger.set_level = logger.setLevel
     logger.is_enabled_for = logger.isEnabledFor
-    logger.get_child = lambda *args, **kwargs: _monkeypatch_logger(
+    logger.get_child = lambda *args, **kwargs: _monkeypatch_logger( # type: ignore
         logger.getChild(*args, **kwargs)
     )
-    logger.get_children = lambda *args, **kwargs: _monkeypatch_logger(
+    logger.get_children = lambda *args, **kwargs: _monkeypatch_logger( # type: ignore
         logger.getChildren(*args, **kwargs)
     )
     logger.add_filter = logger.addFilter
@@ -98,24 +104,24 @@ def _monkeypatch_logger(logger):
         logger.__class__.__name__,
         (logger.__class__,),
         dict(
-            __getattribute__=lambda self, name: (
-                object.__getattribute__(self, name)
+            __getattribute__=lambda self, name: ( # type: ignore
+                object.__getattribute__(self, name) # type: ignore
                 if name != "parent"
-                else _monkeypatch_logger(object.__getattribute__(self, "parent"))
+                else _monkeypatch_logger(object.__getattribute__(self, "parent")) # type: ignore
             )
         ),
     )
 
-    logger.__KISESI__ = True
+    logger.__KISESI__ = True # type: ignore
 
     return logger
 
 
-def get_logger(name=None):
+def get_logger(name: str | None = None):
     return _monkeypatch_logger(getLogger(name))
 
 
-def basic_config(*, incdate=False, use12h=True, **kwargs):
+def basic_config(*, incdate: bool = False, use12h: bool = True, **kwargs: t.Any):
     if not kwargs.get("format"):
         kwargs["format"] = KISESI_DEFAULT_FORMAT_STRING
 
